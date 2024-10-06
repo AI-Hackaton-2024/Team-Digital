d1a65748ff2b12903633cc6fda6c91c48bff9ff0
Konstantin-Natev <konstantin@nolimit.software>
2024-10-06 11:00:31 +0300

    feat: redesign

--- client/src/components/pages/Register Page/RegisterPage.jsx
+++ client/src/components/pages/Register Page/RegisterPage.jsx
@@ -13,32 +13,63 @@ function RegisterPage({ formData, setFormData }) {
     const { name, value } = event.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
-
   return (
     <div className={styles.containerForm}>
-        <h2>Register</h2>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Full Name</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required style = {{color: "black"}}/>
-          
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required style = {{color: "black"}} />
-          
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required style = {{color: "black"}} />
-          
-          <label htmlFor="companyName">Company Name</label>
-          <input type="text" id="companyName" name="companyName" value={formData.companyName} onChange={handleChange} required style = {{color: "black"}} />
-          
-          <div style = {{display: "flex", justifyContent: "space-between"}}>
-            <button onClick = {() => {navigate("/")}}>Back</button>
-            <button type="submit">Sign up</button>
-          </div>
-        </form>
+      <h2 className={styles.heading}>Register</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label className={styles.label} htmlFor="name">Full Name</label>
+        <input
+          className={styles.input}
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="email">Email</label>
+        <input
+          className={styles.input}
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="password">Password</label>
+        <input
+          className={styles.input}
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="companyName">Company Name</label>
+        <input
+          className={styles.input}
+          type="text"
+          id="companyName"
+          name="companyName"
+          value={formData.companyName}
+          onChange={handleChange}
+          required
+        />
+
+        <div className={styles.buttonGroup}>
+          <button className={styles.button} onClick={() => navigate('/')}>Back</button>
+          <button className={styles.button} type="submit">Sign up</button>
+        </div>
+      </form>
     </div>
   );
 }
